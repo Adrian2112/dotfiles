bf2f47733de15335a4d4938f1b3a8b95aab473fd
Adrian Gonzalez <bernardogzzf@gmail.com>
2013-11-25 00:06:12 -0600

    add zsh theme

--- install.sh
+++ install.sh
@@ -29,3 +29,8 @@ for file in $files; do
     echo "Creating symlink to $file in home directory."
     ln -s $dir/$file ~/.$file
 done
+
+# copy theme to zsh dir
+echo "copy agonz theme to zsh theme dir"
+cp agonz.zsh-theme ~/.oh-my-zsh/themes/
+echo "..done"
